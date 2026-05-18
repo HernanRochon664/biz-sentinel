e1f84fc3d990939547e8bc0c4e35dc6afca0c743
Herny664 <hern4n.roch0n@gmail.com>
2026-05-18 19:02:05 -0300

    feat(feature-engineering): add RFM, review, delivery and payment feature nodes

--- src/biz_sentinel/pipelines/feature_engineering/__init__.py
+++ src/biz_sentinel/pipelines/feature_engineering/__init__.py
@@ -1,6 +1,6 @@
 """Feature engineering pipeline for BizSentinel."""
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline  # type: ignore[import-untyped]
 
 
 def create_pipeline(**kwargs) -> Pipeline:
